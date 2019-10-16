cf62a955d3ecf8d8b7f4d763805165c49f5c0257
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-16 12:40:38 +0200

    Add project files.

---
+++ serra giustiniani/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serra_giustiniani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("inserisci i gradi della serra: ");
+            double statoserra = int.Parse(Console.ReadLine());
+            if (statoserra < 5)
+            {
+                Console.WriteLine("serra irrecuperabile");
+            }else
+            {
+                if (statoserra > 18)
+            {
+                Console.WriteLine("la serra è in condizioni ottimali");
+            }else
+            {
+                Console.WriteLine("la serra è in condizioni critiche");
+            }
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
